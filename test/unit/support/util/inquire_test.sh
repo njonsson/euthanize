6a199f9b223fea0c24df1570ddf5b714f2c5f4da
Nils Jonsson <nils@alumni.rice.edu>
2015-04-29 19:37:17 -0500

    Add test coverage for inquire()

---
+++ test/unit/support/util/inquire_test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+source src/support/util.sh
+source test/helper.sh
+
+assert_expression_equal "Doing nothing ... \033[32myes\033[0m"               "inquire 'Doing nothing'"
+assert_expression_equal "Doing something successful ... \033[32myes\033[0m"  'inquire "Doing something successful" ls'
+assert_expression_equal "Doing something unsuccessful ... \033[31mno\033[0m" "inquire \"Doing something unsuccessful\" 'ls nonexistent-file'"
+assert_expression_equal "Doing something unsuccessful ... \033[31mno\033[0m" "inquire 'Doing something unsuccessful' ls \"ls nonexistent-file\""
